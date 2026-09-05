bfd5cab03e540a20646c25099ca14c4498361045
imanuelo <limpongjuan@gmail.com>
2026-09-05 11:47:19 +0700

    Update database configuration

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -84,13 +84,13 @@ WSGI_APPLICATION = 'portofolio.wsgi.application'
 if PRODUCTION:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql1',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.getenv('DB_NAME'),
             'USER': os.getenv('DB_USER'),
             'PASSWORD': os.getenv('DB_PASSWORD'),
             'HOST': os.getenv('DB_HOST'),
             'PORT': os.getenv('DB_PORT'),
-            'OPTION': {
+            'OPTIONS': {
                 'options': f"-c search_path={os.getenv('SCHEMA', 'public')}"
             }
         }
