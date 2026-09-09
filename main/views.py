5b9fdee8281d75e97a831e637fbda208a250f8ac
imanuelo <limpongjuan@gmail.com>
2026-09-09 23:15:09 +0700

    Finish Tutorial 02

---
+++ main/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Juan Imanuel Limpong",
+        "npm": "2506619455",
+        "study_program": "Sistem Informasi",
+        "bio": (
+            "Information Systems student at Universitas Indonesia with a strong interest in"
+            "technology, such as digial product design."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Juan Imanuel Limpong",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
